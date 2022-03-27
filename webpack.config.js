4818ced333c7b448f3747f00b7ce25a051bd25b2
dtrckd <dtrckd@gmail.com>
2022-03-27 18:39:17 +0200

    optimization: remove unsafe_comps that breaks graphpack.

--- webpack.config.js
+++ webpack.config.js
@@ -259,7 +259,7 @@ module.exports = (env, argv) => {
                                 pure_getters: true,
                                 keep_fargs: false,
                                 unsafe: true,
-                                unsafe_comps: true,
+                                unsafe_comps: false, // break graphpack
                                 unsafe_math: true,
                                 pure_funcs: [ 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9'],
                                 //keep_fnames: true,
